5cd2d4a8c94bcf69080a307a21e3ed961d02c8e7
Christopher <caallen@csus.edu>
2023-02-14 15:12:59 -0800

    Merge branch 'annotation-db' of https://github.com/julianofhernandez/Trash-Sorting
     Conflicts:
    	project/annotation_database/db_server.py

--- project/front-end_apps/about.py
+++ project/front-end_apps/about.py
@@ -5,17 +5,58 @@ This gives the about informational section of the program
 Last modified 11/2 by Jeff de Jesus
 '''
 
-about_info = """
-This program is intended to classify trash 
-types in order to determine which trash bin 
-it belongs in. Classifications are done 
-through the use of a computer's webcam, and 
-our program has been tested to be at least 
-75% accurate with it's results."""
+import misc
+
+about_info = """This program is intended to classify trash types in order to determine which 
+trash bin it belongs in. Classifications are done through the use of a computer's webcam, and 
+our program has been tested to be at least 75% accurate with it's results."""
+
+menu_info = """1. Annotate: This is used for adding annotations to images for training.
+2. Classify: This is used to collect data that will be classified.
+3. About: (Current selection) This is used to give users information on the 
+application, explaining what each menu option does, and list developers.
+4. Settings: This is used to change the settings for the application.
+5. Quit: This is used to exit out of the application."""
+
+credit_info = """Developers: Jeffrey de Jesus, Bryan Burch, Christopher Allen, 
+Daniel Smagly, Julian Hernandez, Kenta Miyahara, Santiago Bermudez, 
+Travis Hammond
+
+Project Owner: Dr. Clark Fitzgerald
+"""
+
+menu_options = ['1', '2', '3', '4', 'Q']
+
+menu_prompt = menu_options[0] + ": About Information\n" + \
+              menu_options[1] + ": Menu Information\n" + \
+              menu_options[2] + ": Credits\n" + \
+              menu_options[3] + ": Quit"
+
 
 def main():
-	print("About")
-	print(about_info + "\n")
+	print("About: ")
+	while (True):
+		print("Select what you want information on.")
+		print(menu_prompt)
+
+		key = misc.read_input(menu_options)
+
+		if (key == -1):
+			misc.print_invalid_input()
+			continue
+
+		if (key == menu_options[0]):
+			print(about_info + "\n")
+
+		elif (key == menu_options[1]):
+			print(menu_info + "\n")
+
+		elif (key == menu_options[2]):
+			print(credit_info)
+
+		else:
+			print("Quitting\n")
+			return 0
 
 if __name__ == "__main__":
     main()
--- project/front-end_apps/trash_info.py
+++ project/front-end_apps/trash_info.py
@@ -5,13 +5,58 @@ of trash type, particularly its basic info and how to dispose of it
 Last modified 11/3 by Jeff de Jesus
 '''
 
+import misc
+
+trash_info = """Trash should be disposed of in the nearest trash bin.
+Some examples of trash are, styrofoam, cooking oil, 
+plastic bags, food-soiled paper, or broken ceramics.\n"""
+
+recycle_info = """"Recycle should be placed in the nearest blue recycle bin. "
+Some examples of recycle are plastic bottles, aluminum cans, "
+glass bottles, cardboard, newspapers, or paper bags.\n"""
+
+compost_info = """Compost should be disposed of in the nearest green compost bin. 
+Some examples of compost are fruits, coffee grounds, tea bags, 
+and egg shells. \n"""
+
+menu_options = ['1', '2', '3', '4', 'Q']
+
+menu_prompt = menu_options[0] + ": Trash\n" + \
+              menu_options[1] + ": Recycle\n" + \
+              menu_options[2] + ": Compost\n" + \
+              menu_options[3] + ": Quit"
+
+
 def main():
-	print("This will print info about a specific type of trash")
+    while(True):
+        print("Select which type of trash you want information on.")
+        print(menu_prompt)
+
+        key = misc.read_input(menu_options)
+
+        if(key == -1):
+            misc.print_invalid_input()
+            continue
+
+        if(key == menu_options[0]):
+            print(trash_info)
+
+        elif(key == menu_options[1]):
+            print(recycle_info)
+
+        elif(key == menu_options[2]):
+            print(compost_info)
+
+        else:
+            print("Quitting\n")
+            return 0
+
 
 def print_trash_info(type):
-	'''Will print out the info for a specific type of trash given the type
+    '''Will print out the info for a specific type of trash given the type
 	'''
-	print("Trash Type: ", type)
+    print("Trash Type: ", type)
+
 
 if __name__ == "__main__":
     main()
